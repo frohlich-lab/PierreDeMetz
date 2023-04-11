c79ba4aa569c554b79ff052774b6e635fbe6f243
pierre de metz <demetz.pierre@icloud.com>
2023-04-11 18:50:05 +0200

    function to load weights

--- inst/python/training.py
+++ inst/python/training.py
@@ -81,36 +81,6 @@ def model_training(model, optimizer, weights, opt_state, param_dict, input_data,
 
     return history, model, weights
 
-def generate_batches(input_data, batch_size, rng):
-    """Generate batches for training.
-
-    Args:
-        input_data: A dictionary of NumPy arrays containing the input data.
-        batch_size: The batch size.
-        rng: A JAX PRNGKey.
-
-    Yields:
-        A tuple of (select, fold, bind, target) batches.
-    """
-    num_samples = input_data['select'].shape[0]
-    indices = jnp.arange(num_samples)
-
-    # Shuffle the training data.
-    rng, _ = jax.random.split(rng)
-    indices = jax.random.permutation(rng, indices)
-
-    # Generate batches.
-    for start_idx in range(0, num_samples, batch_size):
-        end_idx = min(start_idx + batch_size, num_samples)
-        batch_indices = indices[start_idx:end_idx]
-
-        batch_select = input_data['select'][batch_indices]
-        batch_fold = input_data['fold'][batch_indices]
-        batch_bind = input_data['bind'][batch_indices]
-        batch_target = input_data['target'][batch_indices]
-
-        yield batch_select, batch_fold, batch_bind, batch_target
-
 def fit_model_grid_jax(param_dict, input_data, n_epochs, rng):
     # Summarize results
     print("Grid search using %s" % (param_dict))
@@ -129,26 +99,6 @@ def fit_model_grid_jax(param_dict, input_data, n_epochs, rng):
         number_additive_traits=param_dict['number_additive_traits'])
 
     #@jax.jit
-    #def loss_fn(params, inputs_select, inputs_folding, inputs_binding, target):
-        #output, folding_additive_layer, binding_additive_layer, folding_additive_trait_layer, binding_additive_trait_layer = model.apply(params, inputs_select, inputs_folding, inputs_binding)
-        #loss = jnp.mean(jnp.abs(output - target))
-        #print(folding_additive_layer)
-        #print(binding_additive_layer)
-        #print(folding_additive_trait_layer)
-        #print(binding_additive_trait_layer)
-
-        # Apply L1 and L2 regularization
-        #l1_loss = 0
-        #l2_loss = 0
-        #print(params['binding_additive_trait']['w'])
-        ##for p in jax.tree_util.tree_leaves(params):
-            #if p.ndim > 1:  # exclude bias parameters
-                #l1_loss += jnp.sum(jnp.abs(p))
-                #l2_loss += jnp.sum(jnp.square(p))
-        #loss = loss + param_dict['l1_regularization_factor'] * l1_loss + param_dict[
-            #'l2_regularization_factor'] * l2_loss
-        #return loss
-
     def loss_fn(params, inputs_select, inputs_folding, inputs_binding, target):
         output, folding_additive_layer, binding_additive_layer, folding_additive_trait_layer, binding_additive_trait_layer = model.apply(params, inputs_select, inputs_folding, inputs_binding)
         loss = jnp.mean(jnp.abs(output - target))
---
+++ inst/python/weights_loading.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy
+import os
+import jax.numpy as jnp
+
+def weights_loading(output_directory, model_count):
+
+    weights_filepath = os.path.join(output_directory, "model_weights_"+str(model_count)+".txt")
+    weights_dataframe = pd.read_csv(weights_filepath, sep="\t")
+
+    weights_dataframe = weights_dataframe.dropna(subset=['binding_coefficient'])
+
+
+    # Extract the folding and binding coefficients
+    folding_coefficients = weights_dataframe['folding_coefficient'].to_numpy()
+    binding_coefficients = weights_dataframe['binding_coefficient'].to_numpy()
+
+    # Load the remaining model parameters (linear layers) from the separate text file
+    parameters_filepath = os.path.join(output_directory, "model_parameters_"+str(model_count)+".txt")
+
+    loaded_linear_params = {}
+    with open(parameters_filepath, 'r') as f:
+        while True:
+            module_name = f.readline().strip()
+            if not module_name:
+                break
+            w = float(f.readline().strip())
+            f.readline()  # Skip the next line containing the bias name
+            b = float(f.readline().strip())
+            loaded_linear_params[module_name] = {'w': jnp.array([[w]]), 'b': jnp.array([b])}
+
+    # Create a dictionary with the loaded weights
+    loaded_weights = {
+        'folding_additive_trait': {
+            'w': folding_coefficients.reshape(-1, 1)
+        },
+        'folding_additive': loaded_linear_params['folding_linear_kernel'],
+        'binding_additive_trait': {
+            'w': binding_coefficients.reshape(-1, 1)
+        },
+        'binding_additive': loaded_linear_params['binding_linear_kernel']
+    }
+    return loaded_weights
