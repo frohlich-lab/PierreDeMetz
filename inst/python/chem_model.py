461141278615bf101f73c46d72bd31cab53ab791
pierre de metz <demetz.pierre@icloud.com>
2023-05-02 16:22:41 +0100

    clearing tree

--- inst/python/chem_model.py
+++ inst/python/chem_model.py
@@ -2,7 +2,7 @@ import jax.numpy as jnp
 from jax import grad, jit, vmap
 from jax.scipy.optimize import minimize
 
-def constant_calc(delta_g_df, delta_g_db=None, l=None):
+def constant_calc_tri_state(delta_g_df, delta_g_db=None, l=None):
 
     # Gas constant and temperature
     R, T = 8.314, 298.15
@@ -17,6 +17,18 @@ def constant_calc(delta_g_df, delta_g_db=None, l=None):
     k_bm = 1.0
     k_b_minus = k_fm * l / K_db
     return k_fp, k_fm, k_bm, k_b_minus
+
+def constant_calc_two_state(delta_g_df):
+    # Gas constant and temperature
+    R, T = 8.314, 298.15
+
+    # Calculate the equilibrium constants
+    K_df = jnp.exp(-delta_g_df / (R * T))
+    # Set arbitrary rate constants and calculate the others based on the equilibrium constants
+    k_fp = 1.0
+    k_fm = k_fp * K_df
+
+    return k_fp, k_fm
 
 # Penalty function
 def penalty(x, penalty_weight=1e6):
@@ -37,7 +49,7 @@ def objective_tri_state(x, k_fp, k_fm, k_bm, k_b_minus,l):
 
 def opt_soln_tri_state(delta_g_df, delta_g_db, l=1.0):
 
-    k_fp, k_fm, k_bm, k_b_minus = constant_calc(delta_g_df, delta_g_db, l)
+    k_fp, k_fm, k_bm, k_b_minus = constant_calc_tri_state(delta_g_df, delta_g_db, l)
 
     # Initial guess
     x0 = jnp.array([1/3, 1/3, 1/3])
@@ -49,8 +61,23 @@ def opt_soln_tri_state(delta_g_df, delta_g_db, l=1.0):
 
     return result.x
 
-def objective_two_state():
-    pass
+def objective_two_state(x,k_fp, k_fm):
 
-def opt_soln_two_state():
-    pass
+    x_o, x_f = x
+    f_xo = -k_fp * x_o + k_fm * x_f
+    f_xf = 1-f_xo
+
+    return jnp.square(f_xo) + jnp.square(f_xf) + penalty(x)
+
+def opt_soln_two_state(delta_g_df):
+
+    k_fp, k_fm = constant_calc_two_state(delta_g_df)
+    # Initial guess
+    x0 = jnp.array([1/3, 1/3, 1/3])
+
+    # Minimize the objective function
+    result = minimize(objective_two_state, x0, method='BFGS', args=(k_fp, k_fm))
+
+    print("Optimal solution:", result.x)
+
+    return result.x
