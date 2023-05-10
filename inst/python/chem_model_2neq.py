1325db4381ce253dd339e19cebb2c77fd9ad19cd
pierre de metz <demetz.pierre@icloud.com>
2023-05-10 18:06:14 +0100

    updated structure

--- inst/python/chem_model_2neq.py
+++ inst/python/chem_model_2neq.py
@@ -7,11 +7,27 @@ from jax import vmap
 from diffrax import diffeqsolve, ODETerm, Dopri5
 
 ################# TWO STATE NON EQ MODEL IMPLICIT
+def objective_two_state_noneq_implicit():
+    pass
+
+def objective_and_grad_two_state_noneq_implicit():
+    pass
+
+def opt_soln_two_state_noneq_implicit():
+    pass
 
 def two_state_noneq_implicit_vec():
     pass
 
 ################# TWO STATE NON EQ MODEL ODE
+def objective_two_state_noneq_ODE():
+    pass
+
+def objective_and_grad_two_state_noneq_ODE():
+    pass
+
+def opt_soln_two_state_noneq_ODE():
+    pass
 
 def two_state_noneq_ode_vec():
     pass
