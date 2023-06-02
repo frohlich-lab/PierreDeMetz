2ae5b88ecb629a70c5c75c0f35c116ea94cb7069
pierre de metz <demetz.pierre@icloud.com>
2023-06-02 13:35:43 +0100

    working implicit but not ODE

--- inst/python/chemical_models.py
+++ inst/python/chemical_models.py
@@ -14,23 +14,22 @@ class ChemicalModel(eqx.Module):
     def __init__(self, is_implicit):
         self.is_implicit = is_implicit
 
-    def objective_and_grad(self, objective, x, args):
+    def objective_and_grad(self, x, objective, args):
         objective_value = objective(x, args)
         grad = jax.grad(objective)(x, args)
         return objective_value, grad
 
-    def solve_implicit(self, x0, args, objective):
-        bfgs = BFGS(maxiter=1000, fun=self.objective_and_grad, value_and_grad=True)
+    def solve_implicit(self, x0, objective, args):
+        bfgs = BFGS(maxiter=1000, fun=lambda x: self.objective_and_grad(x, objective, args), value_and_grad=True)
         try:
-            result = bfgs.run(objective, x0, args)
-            print(f'Successfully solved for {result.x}')
-            return result.x
-
+            result = bfgs.run(x0)
+            return result.params
         except Exception as e:
             print(f'Error occurred while solving: {e}')
             return None
 
-    def solve_ode(self, objective, x0, args,t0=0, t1=11, dt0=0.1):
+    def solve_ode(self, x0, objective, args ,t0=0, t1=10, dt0=0.1):
+        print(type(objective))
         term = ODETerm(objective)
         solver = Dopri5()
         solution = diffeqsolve(term, solver, t0=t0, t1=t1, dt0=dt0, y0=x0, args=args)
@@ -43,19 +42,12 @@ class ChemicalModel(eqx.Module):
 
     def opt_vectorize(self, x0, args, objective):
         if self.is_implicit:
-            solve_implicit_vectorized = vmap(self.solve_implicit)
-            results = solve_implicit_vectorized(x0, args, objective)
+            solve_implicit_vectorized = vmap(self.solve_implicit, in_axes=(None, None, 0))
+            results = solve_implicit_vectorized(x0, objective, args)
         else:
-            solve_ode_vectorized = vmap(self.get_steady_state_solution)
-            results = solve_ode_vectorized(objective, x0, args)
+            solve_ode_vectorized = vmap(self.get_steady_state_solution, in_axes=(None, None, 0))
+            results = solve_ode_vectorized(x0, objective, args)
         return results
-
-    def solve(self, x0, args, objective=None):
-        if self.is_implicit:
-            return self.opt_vectorize(x0, args, objective)
-        else:
-            return self.opt_vectorize(objective, x0, args)
-
 
 class ThreeStateEquilibrium(ChemicalModel):
     x0_two: jnp.ndarray
@@ -69,54 +61,44 @@ class ThreeStateEquilibrium(ChemicalModel):
     def objective_folding(self, x, args):
         delta_g_df = args
         x_o, x_f = x
-
         total_conc = 1 - x_o - x_f
         f_xo = -x_o * jnp.exp(-delta_g_df) + x_f
         f_xf = x_o * jnp.exp(-delta_g_df) - x_f
-
         if not self.is_implicit:
             return jnp.array([f_xo, f_xf]).reshape(-1,)
-
         else:
             result = jnp.square(f_xo) + jnp.square(f_xf)+jnp.square(total_conc)
             return jnp.squeeze(result)
 
     def objective_binding(self, x, args):
-        l, delta_g_df, delta_g_db = args
+        delta_g_df, delta_g_db = args
         x_o, x_f, x_b = x
         total_conc = 1 - x_o - x_f - x_b
-
         f_xo = -x_o * jnp.exp(-delta_g_df) + x_f
         f_xb = x_f * jnp.exp(-delta_g_db) - x_b
         f_xf = -f_xo - f_xb
-
         if not self.is_implicit:
             return jnp.array([f_xo, f_xf, f_xb]).reshape(-1,)
-
         else:
             result = jnp.square(f_xo) + jnp.square(f_xf) + jnp.square(f_xb) + jnp.square(total_conc)
-            print(result)
             return jnp.squeeze(result)
 
-    def solve_systems(self, args_folding, args_binding):
-        if self.is_implicit:
-            solution_folding = self.solve(self.x0_two, args=args_folding, objective=self.objective_folding)
-            solution_binding = self.solve(self.x0_tri, args=args_binding, objective=self.objective_binding)
-        else:
-            solution_folding = self.solve(self.x0_two, args=args_folding, objective=self.objective_folding)
-            solution_binding = self.solve(self.x0_tri, args=args_binding, objective=self.objective_binding)
+    def solve_folding(self, args_folding):
+        results = self.opt_vectorize(self.x0_two, args_folding, self.objective_folding)
+        return results[0][1]
 
-
-        return solution_folding, solution_binding
-
+    def solve_binding(self, args_binding):
+        results = self.opt_vectorize(self.x0_tri, args_binding, self.objective_binding)
+        return results[0][2]
 
 if __name__ == '__main__':
-
-    three_state_model = ThreeStateEquilibrium(True)
+    three_state_model = ThreeStateEquilibrium(False)
 
     args_folding = (jnp.array([0.5, 0.2]))
     args_binding = (jnp.array([0.5, 0.2]), jnp.array([0.5, 0.2]))
 
-    solution_folding, solution_binding = three_state_model.solve_systems(args_folding, args_binding)
+    solution_folding = three_state_model.solve_folding(args_folding)
+    solution_binding = three_state_model.solve_binding(args_binding)
+
     print("Solution for folding:", solution_folding)
     print("Solution for binding:", solution_binding)
