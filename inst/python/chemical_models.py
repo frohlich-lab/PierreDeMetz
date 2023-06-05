a03ac9dace929fd8faa87f57b92b3c182c46eb24
pierre de metz <demetz.pierre@icloud.com>
2023-06-05 21:48:26 +0300

    test implementation

--- inst/python/chemical_models.py
+++ inst/python/chemical_models.py
@@ -10,9 +10,13 @@ import equinox as eqx
 
 class ChemicalModel(eqx.Module):
     is_implicit: bool
+    is_degradation: bool
+    is_synthesis: bool
 
-    def __init__(self, is_implicit):
+    def __init__(self, is_implicit, is_degradation, is_synthesis):
         self.is_implicit = is_implicit
+        self.is_degradation = is_degradation
+        self.is_synthesis = is_synthesis
 
     def objective_and_grad(self, x, objective, args):
         objective_value = objective(x, args)
@@ -52,12 +56,12 @@ class ThreeStateEquilibrium(ChemicalModel):
     x0_two: jnp.ndarray
     x0_tri: jnp.ndarray
 
-    def __init__(self, is_implicit):
-        super().__init__(is_implicit)
+    def __init__(self, is_implicit, is_degradation,is_synthesis):
+        super().__init__(is_implicit, is_degradation,is_synthesis)
         self.x0_two = jnp.array([1/2, 1/2])
         self.x0_tri = jnp.array([1/3, 1/3, 1/3])
 
-    def objective_folding(self, *args):#, **kwargs):
+    def objective_folding(self, *args):
         if not self.is_implicit:
             t, x, args = args
         else:
@@ -73,7 +77,7 @@ class ThreeStateEquilibrium(ChemicalModel):
             result = jnp.square(f_xo) + jnp.square(f_xf)+jnp.square(total_conc)
             return jnp.squeeze(result)
 
-    def objective_binding(self, *args):#:, **kwargs):
+    def objective_binding(self, *args):
         if not self.is_implicit:
             t, x, args = args
         else:
@@ -98,8 +102,143 @@ class ThreeStateEquilibrium(ChemicalModel):
         results = self.opt_vectorize(self.x0_tri, args_binding, self.objective_binding)
         return results.T[:][2]
 
+class TwoStateNonEquilibrium(ChemicalModel):
+    def __init__(self, is_implicit, is_degradation,is_synthesis):
+        super().__init__(is_implicit, is_degradation,is_synthesis)
+        self.x0_two = jnp.array([1/2])
+        self.x0_tri = jnp.array([1])
+
+    def objective_folding(self, *args):
+        if not self.is_implicit:
+            t, x, args = args
+        else:
+            x, args = args
+
+        delta_g_df = args
+        x_f = x
+        f_xf = jnp.exp(-delta_g_df) - x_f
+
+        if not self.is_implicit:
+            return jnp.array([f_xf]).reshape(-1,)
+        else:
+            result = jnp.square(f_xf)
+            return jnp.squeeze(result)
+
+    def objective_binding(self, *args):
+        if not self.is_implicit:
+            t, x, args = args
+        else:
+            x, args = args
+        if self.is_degradation == False:
+            delta_g_df, delta_g_db = args
+        else:
+            delta_g_df, delta_g_db, delta_g_dd = args
+        x_f, x_b = x
+        l = 1.0
+
+        if self.is_degradation==False:
+            f_xb = -x_b * jnp.exp(delta_g_db) + x_f * l
+            f_xf = jnp.exp(-delta_g_df) - x_f - f_xb
+
+        else:
+            f_xb = -x_b * jnp.exp(delta_g_db) + x_f * l - x_b * jnp.exp(delta_g_dd)
+            f_xf = jnp.exp(-delta_g_df) - x_f - f_xb + x_b * jnp.exp(delta_g_dd)
+
+
+        if not self.is_implicit:
+            return jnp.array([f_xf, f_xb]).reshape(-1,)
+        else:
+            result = jnp.square(f_xf) + jnp.square(f_xb)
+            return jnp.squeeze(result)
+
+
+class ThreeStateNonEquilibrium(ChemicalModel):
+    def __init__(self, is_implicit, is_degradation,is_synthesis):
+        super().__init__(is_implicit, is_degradation,is_synthesis)
+        self.x0_two = jnp.array([1/2, 1/2])
+        self.x0_tri = jnp.array([1/3, 1/3, 1/3])
+
+    def objective_folding(self, *args):
+        if not self.is_implicit:
+            t, x, args = args
+        else:
+            x, args = args
+        if self.is_synthesis == False:
+            delta_g_df = args
+        else:
+            delta_g_do, delta_g_df = args
+
+        x_o, x_f = x
+        flux = 1.0
+        if self.is_synthesis == True:
+            f_xo = flux * jnp.exp(delta_g_do) + x_f * jnp.exp(delta_g_df) -2 * x_o
+            f_xf = x_o * jnp.exp(-delta_g_df) - x_f
+        else:
+            f_xo =  x_f * jnp.exp(delta_g_df) -2 * x_o
+            f_xf = x_o * jnp.exp(-delta_g_df) - x_f
+
+
+        if not self.is_implicit:
+            return jnp.array([f_xo, f_xf]).reshape(-1,)
+        else:
+            result = jnp.square(f_xo) + jnp.square(f_xf)
+            return jnp.squeeze(result)
+
+    def objective_binding(self, *args):
+        if not self.is_implicit:
+            t, x, args = args
+        else:
+            x, args = args
+        if self.is_degradation == False and self.is_synthesis == False:
+            delta_g_df, delta_g_db = args
+        elif self.is_degradation == True and self.is_synthesis == False:
+            delta_g_df, delta_g_db, delta_g_dd = args
+        elif self.is_degradation == False and self.is_synthesis == True:
+            delta_g_df, delta_g_db, delta_g_do = args
+        else:
+            delta_g_df, delta_g_db,delta_g_dd, delta_g_do = args
+
+        x_o, x_f, x_b = x
+
+        flux = 1.0
+        l = 1.0
+
+        if self.is_degradation == False and self.is_synthesis == False:
+            f_xo = flux * jnp.exp(delta_g_do) + x_f * jnp.exp(delta_g_df) -2 * x_o
+            f_xb = -x_b * jnp.exp(delta_g_db) + x_f * l
+            f_xf = x_o * jnp.exp(-delta_g_df) - x_f - f_xb
+        elif self.is_degradation == True and self.is_synthesis == False:
+            f_xo =  x_f * jnp.exp(delta_g_df) -2 * x_o
+            f_xb = -x_b * jnp.exp(delta_g_db) + x_f * l - x_b * jnp.exp(delta_g_dd)
+            f_xf = x_o * jnp.exp(-delta_g_df) - x_f - f_xb  - x_b * jnp.exp(delta_g_dd)
+        elif self.is_degradation == False and self.is_synthesis == True:
+            f_xo = flux * jnp.exp(delta_g_do) + x_f * jnp.exp(delta_g_df) -2 * x_o
+            f_xb = -x_b * jnp.exp(delta_g_db) + x_f * l
+            f_xf = x_o * jnp.exp(-delta_g_df) - x_f - f_xb
+        else:
+            f_xo = flux * jnp.exp(delta_g_do) + x_f * jnp.exp(delta_g_df) -2 * x_o
+            f_xb = -x_b * jnp.exp(delta_g_db) + x_f * l - x_b * jnp.exp(delta_g_dd)
+            f_xf = x_o * jnp.exp(-delta_g_df) - x_f - f_xb + - x_b * jnp.exp(delta_g_dd)
+
+
+        if not self.is_implicit:
+            return jnp.array([f_xo, f_xf, f_xb]).reshape(-1,)
+        else:
+            result = jnp.square(f_xo) + jnp.square(f_xf) + jnp.square(f_xb)
+            return jnp.squeeze(result)
+
+    def solve_folding(self, args_folding):
+        results = self.opt_vectorize(self.x0_two, args_folding, self.objective_folding)
+        return results.T[:][2]
+
+    def solve_binding(self, args_binding):
+        results = self.opt_vectorize(self.x0_tri, args_binding, self.objective_binding)
+        return results.T[:][2]
+
 if __name__ == '__main__':
-    three_state_model = ThreeStateEquilibrium(False)
+    three_state_model = ThreeStateEquilibrium(is_implicit = False,
+                                              is_degradation = False,
+                                              is_synthesis=False)
 
     args_folding = (jnp.array([0.5, 0.2]))
     args_binding = (jnp.array([0.5, 0.2]), jnp.array([0.5, 0.2]))
